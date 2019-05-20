36e14d8fb2761d45eaa9ba6d1e061a0cc65ebe16
PANGAPING <39149430+PANGAPING@users.noreply.github.com>
2019-05-20 13:25:15 +0800

    Add files via upload

---
+++ ASP.NET/day2/Event.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = new MyEvent(5);
+            e.SetValue(10);
+
+            //generate the event handler
+            e.ChangeNum += new MyEvent.NumManipulationHandler(MyEvent.EventFired);
+
+            e.SetValue(200);
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class MyEvent
+    {
+        private int value;
+
+
+        // delegate
+        public delegate void NumManipulationHandler();
+
+        //The method recommoned.
+        public delegate void ChangedEventhandler(object sender, EventArgs e);
+
+
+        //Set the event
+        public event NumManipulationHandler ChangeNum;
+
+        public MyEvent(int n)
+        {
+            SetValue(n);
+        }
+
+        public static void EventFired()
+        {
+            Console.WriteLine("Binded Event fired");
+        }
+
+        protected virtual void OnNumChanged()
+        {
+            if (ChangeNum != null)
+            {
+                ChangeNum();
+            }
+            else
+            {
+                Console.WriteLine("Event fired!");
+            }
+        }
+
+
+        public void SetValue(int n)
+        {
+            if (value != n)
+            {
+                value = n;
+                OnNumChanged();
+            }
+        }
+    }
+}
---
+++ ASP.NET/day2/Indexer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MyIndexer
+    {
+
+        static void Main(string[] args) {
+
+            var names = new IndexedNames();
+            names[1] = "dengweihao";
+
+        }
+
+    }
+
+
+    class IndexedNames {
+        private string[] nameList = new string[10];
+
+        public IndexedNames()
+        {
+            for (int i = 0; i < nameList.Length; i++) {
+                nameList[i] = "N/A";
+            }
+        }
+
+        public string this[int index]
+        {
+
+            get
+            {
+                string tmp;
+                if (index >= 0 && index <= nameList.Length - 1)
+                {
+                    tmp = nameList[index];
+                }
+                else {
+                    tmp = "";
+                }
+
+                return tmp;
+            }
+
+            set
+            {
+
+                if (index >= 0 && index < nameList.Length - 1) {
+                    nameList[index] = value;
+                }
+            }
+        }
+
+        public int this[string name] {
+            get {
+                int index = 0;
+                while (index < nameList.Length) {
+                    if (nameList[index] == name) {
+                        return index;
+                    }
+
+                    index++;
+                }
+                return -1;
+            }
+        }
+    }
+
+
+    public interface ISomeInterface {
+        int this[int index] {
+            get;
+            set;
+        }
+    }
+
+    public class IndexerClass1 : ISomeInterface {
+        private int[] arr = new int[100];
+
+        public int this[int index] {
+            get {
+                return arr[index];
+            }
+            set {
+
+            }
+        }
+    }
+}
