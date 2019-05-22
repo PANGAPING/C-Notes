3be06856f349a4ddab3f81fd5cd9f98224655927
PANGAPING <39149430+PANGAPING@users.noreply.github.com>
2019-05-22 12:14:45 +0800

    Add files via upload

---
+++ ASP.NET/day3/attribute.cs
@@ -0,0 +1,95 @@
+
+// define a user attribute
+[AttributeUsage(AttributeTargets.Class |
+AttributeTargets.Constructor |
+AttributeTargets.Field |
+AttributeTargets.Method |
+AttributeTargets.Property,
+AllowMultiple = true)]
+
+public class DeBugInfo : System.Attribute
+{
+  private int bugNo;
+  private string developer;
+  private string lastReview;
+  public string message;
+
+  public DeBugInfo(int bg, string dev, string d)
+  {
+      this.bugNo = bg;
+      this.developer = dev;
+      this.lastReview = d;
+  }
+
+  public int BugNo
+  {
+      get
+      {
+          return bugNo;
+      }
+  }
+  public string Developer
+  {
+      get
+      {
+          return developer;
+      }
+  }
+  public string LastReview
+  {
+      get
+      {
+          return lastReview;
+      }
+  }
+  public string Message
+  {
+      get
+      {
+          return message;
+      }
+      set
+      {
+          message = value;
+      }
+  }
+}
+
+
+//use it
+[DeBugInfo(45, "Zara Ali", "12/8/2012", Message = "Return type mismatch")]
+[DeBugInfo(49, "Nuha Ali", "10/10/2012", Message = "Unused variable")]
+class Rectangle
+{
+  // 成员变量
+  protected double length;
+  protected double width;
+  public Rectangle(double l, double w)
+  {
+      length = l;
+      width = w;
+  }
+  [DeBugInfo(55, "Zara Ali", "19/10/2012",
+  Message = "Return type mismatch")]
+  public double GetArea()
+  {
+      return length * width;
+  }
+  [DeBugInfo(56, "Zara Ali", "19/10/2012")]
+  public void Display()
+  {
+      Console.WriteLine("Length: {0}", length);
+      Console.WriteLine("Width: {0}", width);
+      Console.WriteLine("Area: {0}", GetArea());
+  }
+}
+
+
+//Obsolete pre_attribute
+//Info user that the method is old,
+//Maybe he should use the new one.
+
+[Obsolete(
+   message,
+   iserror
+)]
