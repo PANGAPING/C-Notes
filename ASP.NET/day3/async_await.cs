3bcabfb5da99d85dbb77175fff0d54c6731bc2c3
PANGAPING <39149430+PANGAPING@users.noreply.github.com>
2019-05-22 20:56:47 +0800

    Add files via upload

---
+++ ASP.NET/day3/async_await.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doSomethingAsyn();
+        }
+
+        
+
+        static async void doSomethingAsyn() {
+            Task<int> task = new Task<int>(getNumber);
+
+            task.Start();
+
+            Console.WriteLine("Processing data.");
+
+            int count = await task;
+
+            Console.WriteLine("The result is"+task);
+
+        }
+
+        static int getNumber() {
+            return 9;
+        }
+    }
+}
