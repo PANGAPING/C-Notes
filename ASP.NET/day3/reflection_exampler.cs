71274a13bef563a90dae71a0c6684383bb7ce13c
PANGAPING <39149430+PANGAPING@users.noreply.github.com>
2019-05-22 16:06:49 +0800

    Add files via upload

---
+++ ASP.NET/day3/reflection_exampler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+            Type customerType = executingAssembly.GetType("Customer");
+
+            object customerInstance = Activator.CreateInstance(customerType);
+
+            MethodInfo getFullNameMethod = customerType.GetMethod("GetFullName");
+
+            string[] parameters = new string[2];
+
+            parameters[0] = "WeiHao";
+            parameters[1] = "Deng";
+
+            string fullName = (string)getFullNameMethod.Invoke(customerInstance, parameters);
+
+            Console.WriteLine("The result is " + fullName);
+
+
+        }
+    }
+
+
+    public class Customer {
+        string GetFullName(string firstName, string lastName) {
+            return firstName + lastName;
+        }
+
+    }
+}
